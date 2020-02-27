0d0456cefdb027028dbbc66636d3c0e6c9a431d0
rehkalainin <rkonstantin@gmail.com>
2020-02-27 15:23:45 +0200

    first commit

---
+++ build.sbt
@@ -0,0 +1,28 @@
+import sbt._
+import Dependencies.allDependencies
+
+name := "mentor-space"
+version := "0.1"
+scalaVersion := "2.13.1"
+
+resolvers += Resolver.jcenterRepo
+
+lazy val domain = project
+  .settings(libraryDependencies ++= allDependencies)
+
+lazy val serialization = project
+  .dependsOn(domain)
+
+lazy val service = project
+  .dependsOn(serialization)
+
+lazy val api = project
+  .dependsOn(service)
+
+lazy val boot = project
+  .dependsOn(api)
+
+lazy val `mentor-space` = Project("root", file("."))
+  .aggregate(domain, serialization, service, api, boot)
+  .settings(moduleName := "mentor-space")
+  .settings(name := "mentor-space")
---
+++ project/Dependencies.scala
@@ -0,0 +1,49 @@
+import sbt._
+
+object Dependencies {
+
+  val akkaVersion = "2.5.29"
+  val scalaTestVersion = "3.1.0"
+  val levelDbVersion = "0.12"
+  val leveldbjniVersion = "1.8"
+  val postgresqlVersion = " 42.2.10"
+  val akkaPersistenceJdbcVersion = "3.5.3"
+  val akkaCassandraVersion = "0.102"
+  val akkaCassandraLauncherVersion = "0.102"
+  val protobufJavaVersion = "3.11.4"
+
+
+  val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion
+  val akkaActor = "com.typesafe.akka" %% "akka-actor" % akkaVersion
+  val akkaTestKit = "com.typesafe.akka" %% "akka-testkit" % akkaVersion
+  val akkaPersistence = "com.typesafe.akka" %% "akka-persistence" % akkaVersion
+  val levelDB = "org.iq80.leveldb" % "leveldb" % levelDbVersion
+  val leveldbjni = "org.fusesource.leveldbjni" % "leveldbjni-all" % leveldbjniVersion
+
+
+  // JDBC with PostgreSQL
+  val postgresql = "org.postgresql" % "postgresql" % "42.2.10"
+  val akkaPersistenceJdbc = "com.github.dnvriend" %% "akka-persistence-jdbc" % akkaPersistenceJdbcVersion
+
+  // Cassandra
+  val akkaCassandra = "com.typesafe.akka" %% "akka-persistence-cassandra" % akkaCassandraVersion
+  val akkaCassandraLauncher = "com.typesafe.akka" %% "akka-persistence-cassandra-launcher" % akkaCassandraLauncherVersion % Test
+
+
+  // Google Protocol Buffers
+  val protobufJava = "com.google.protobuf" % "protobuf-java" % protobufJavaVersion
+
+  val allDependencies = Seq(
+    akkaActor,
+    akkaTestKit,
+    akkaPersistence,
+    scalaTest,
+    levelDB,
+    leveldbjni,
+    postgresql,
+    akkaPersistenceJdbc,
+    akkaCassandra,
+    akkaCassandraLauncher,
+    protobufJava
+  )
+}
